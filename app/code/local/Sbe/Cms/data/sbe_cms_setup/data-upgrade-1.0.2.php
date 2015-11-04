d21107c1bc7431243bdeaed25a80f2a909ebd11a
Thomas Zborowski <tzborowski@sbe-online.com>
2015-11-04 15:28:22 +0100

    changement version xml2

--- app/code/local/Sbe/Cms/data/sbe_cms_setup/data-upgrade-1.0.2.php
+++ app/code/local/Sbe/Cms/data/sbe_cms_setup/data-upgrade-1.0.2.php
@@ -1,46 +1,42 @@
 <?php
 
+$this->startSetup();
 
-$cmsContent="Contenu du bloc test Thomas";
 
-$installer->run("
+$this->run("
     
-    INSERT INTO `{$this->getTable('cms_page')}`
-        (`title`,
-        `root_template`,
-        `meta_keywords`,
-        `meta_description`,
-        `identifier`,
-        `content_heading`,
-        `content`,
-            `creation_time`,
-            `update_time`,
-            `is_active`,
-            `sort_order`,
-            `layout_update_xml`,
-            `custom_theme`,
-            `custom_theme_from`,
-            `custom_theme_to`)
-    VALUES (
-    'block_product_tab_thomas',
-    'two_columns_right',
-    'block_product_tab_thomas'
-    'block_product_tab_thomas',
-    'block_product_tab_thomas',
-    'block_product_tab_thomas',
-     $cmsContent,
-     now(),
-     now(),
-     1,
-     0,
-     NULL,
-     NULL,
-     NULL,
-     NULL);
+    INSERT INTO `cms_page` (
+     `title`, 
+     `root_template`, 
+     `meta_keywords`, 
+     `meta_description`, 
+     `identifier`, 
+     `content_heading`, 
+     `content`, 
+     `creation_time`, 
+     `update_time`, 
+     `is_active`, 
+     `sort_order`, 
+     `layout_update_xml`, 
+     `custom_theme`, 
+     `custom_root_template`, 
+     `custom_layout_update_xml`, 
+     `custom_theme_from`, 
+     `custom_theme_to`, 
+     `meta_title`, 
+     `mageworx_hreflang_identifier`, 
+     `meta_robots`, 
+     `exclude_from_sitemap`) VALUES
+('block_product_tab_thomas', 
+'two_columns_right', 
+'block_product_tab_thomas keywords', 
+'block_product_tab_thomas', 
+'no-route', 
+NULL,'block_product_tab_thomas contenu', now(), now(), 1, 0, NULL, NULL, NULL, NULL, NULL, NULL, '', '', '', 0)
+");
 
-    INSERT INTO `{$this->getTable('cms/page_store')}` (`page_id`,
-`store_id`) VALUES
-    (LAST_INSERT_ID(), ".$storeId.");
-    ");
-}
+$this->endSetup();
+
+
+
 ?>
