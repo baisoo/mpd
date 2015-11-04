ee903f32c3a8309da2eb7676c0cac4ffacdce4ab
Thomas Zborowski <tzborowski@sbe-online.com>
2015-11-04 15:35:16 +0100

    changement version xml2

--- app/code/local/Sbe/Cms/data/sbe_cms_setup/data-upgrade-1.0.1.php
+++ app/code/local/Sbe/Cms/data/sbe_cms_setup/data-upgrade-1.0.1.php
@@ -5,7 +5,6 @@ $this->startSetup();
 
 $this->run("
 
-
 INSERT INTO `cms_block` (`title`, `identifier`, `content`, `creation_time`, `update_time`, `is_active`) 
 VALUES
 ('block_product_tab_thomas', 'block_product_tab_thomas', 'block_product_tab_thomas contenu', now(), now(), 1);
